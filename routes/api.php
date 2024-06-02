f756f36f1225cb04b1bf21403c2532c6beaf25b7
Matheus Rodrigues <matheus.rodrisantos@outlook.com>
2024-06-02 15:25:33 -0300

    rota de lista todas as paroquias

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\PedidoController;
+use App\Http\Controllers\API\V1\ParoquiaController;
 
 Route::prefix('v1')->group(function()
 {
     Route::prefix('pedido')->group(function(){
         Route::post('/store',[PedidoController::class,'store']);
+    });
+
+    Route::prefix('paroquia')->group(function(){
+        Route::get('/list-all',[ParoquiaController::class,'index']);
     });
 });
