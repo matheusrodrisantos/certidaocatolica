c5d65fbcf401c98b1221ffdfa069cba8cc7ab935
Matheus Rodrigues <matheus.rodrisantos@outlook.com>
2024-06-06 23:16:36 -0300

    sanctum config with cookie

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V1\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\V1\PedidoController;
@@ -7,11 +8,17 @@ use App\Http\Controllers\API\V1\ParoquiaController;
 
 Route::prefix('v1')->group(function()
 {
+    
     Route::prefix('pedido')->group(function(){
         Route::post('/store',[PedidoController::class,'store']);
     });
 
+  
+    Route::post('/login',[AuthController::class,'login']);
+    Route::post('/register',[AuthController::class,'register']);
+ 
+
     Route::prefix('paroquia')->group(function(){
-        Route::get('/list-all',[ParoquiaController::class,'index']);
+        Route::get('/list-all',[ParoquiaController::class,'index'])->middleware('auth:sanctum');
     });
 });
